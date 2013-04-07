4fd9deb2b45a6b78cd8d3cdaedc7da35432964e9
Vikraman <vikraman.choudhury@gmail.com>
2013-04-08 04:38:30 +0530

    Draw basic board

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import Control.Monad       (void)
+import Control.Monad       (forM_, void)
 import Control.Monad.Trans (liftIO)
 
+import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 
@@ -29,5 +30,25 @@ main = do
   aboutHelpImageMenuItem <- builderGetObject builder castToMenuItem "aboutHelpImageMenuItem"
   aboutHelpImageMenuItem `on` menuItemActivate $ liftIO $ void (dialogRun simAboutDialog)
 
+  simFrame <- builderGetObject builder castToFrame "simFrame"
+  simCanvas <- drawingAreaNew
+  containerAdd simFrame simCanvas
+  simCanvas `on` exposeEvent $ liftIO $ do (w, h) <- widgetGetSize simCanvas
+                                           simDrawing <- widgetGetDrawWindow simCanvas
+                                           renderWithDrawable simDrawing $
+                                             drawBoard (fromIntegral w) (fromIntegral h)
+                                           return False
+
   widgetShowAll simWindow
   mainGUI
+
+drawBoard :: Double -> Double -> Render ()
+drawBoard w h = do
+  let r = 150
+  let t = pi/3
+  forM_ [1..6] $ \x -> do
+    arc (w/2 + r*sin (x*t)) (h/2 + r*cos (x*t)) 15 0 (2*pi)
+    stroke
+    moveTo (w/2 + r*sin(x*t) - 3) (h/2 + r*cos (x*t) + 3)
+    showText $ show $ floor x
+    newPath
